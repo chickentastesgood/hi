76798fb27a451ec8ea2580b5c4912bc0f303d3f9
chickentastesgood <117940931+chickentastesgood@users.noreply.github.com>
2022-11-16 13:55:44 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,94 @@
+basic.showNumber(1)
+basic.showLeds(`
+    . # # # .
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.pause(5000)
+basic.showString("Tree")
+basic.showNumber(2)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # # # #
+    . # . # .
+    . # # # .
+    `)
+basic.pause(5000)
+basic.showString("House")
+basic.showNumber(3)
+basic.showLeds(`
+    . . . . .
+    # . . . #
+    . # . # .
+    . . # . .
+    . . . . .
+    `)
+basic.pause(5000)
+basic.showString("Bird")
+basic.showNumber(4)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+basic.pause(5000)
+basic.showString("Smile")
+basic.showNumber(5)
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    . # . # .
+    # . . . #
+    # . . . #
+    `)
+basic.pause(5000)
+basic.showString("Hito")
+basic.showNumber(6)
+basic.showLeds(`
+    . # . # .
+    # # # # #
+    # . # . #
+    # # # # #
+    # . # . #
+    `)
+basic.pause(5000)
+basic.showString("Ghost")
+basic.showNumber(7)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . # . .
+    . # # # .
+    # # . # #
+    `)
+basic.pause(5000)
+basic.showString("Tepee")
+basic.showNumber(8)
+basic.showLeds(`
+    # # . # #
+    # # # # #
+    . # # # .
+    # # # # #
+    # # . # #
+    `)
+basic.pause(5000)
+basic.showString("X")
+basic.showNumber(9)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # # # # #
+    # . . . #
+    `)
+basic.pause(5000)
+basic.showString("Frown")
+basic.showString("The End")
+basic.forever(function () {
+	
+})
